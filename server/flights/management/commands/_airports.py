2aa6dac3c7fdfd74f8de056f36eaeb65f536c212
Dan <danii-benitez@hotmail.com>
2023-03-08 00:06:43 -0300

    [add] cache to _airports populate script

--- server/flights/management/commands/_airports.py
+++ server/flights/management/commands/_airports.py
@@ -4,20 +4,31 @@ from ...models import Airport, Region, Country
 
 def populate_airports():
     data = []
+    country_cache = {}
+    region_cache = {}
     with open('flights/data/airports.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
 
-            try:
-                country = Country.objects.get(name=row['country'])
+            if row['country'] in country_cache:
+                country = country_cache[row['country']]
+            else:
 
-            except Country.DoesNotExist:
-                country = None
+                try:
+                    country = Country.objects.get(name=row['country'])
+                except Country.DoesNotExist:
+                    country = None
+                country_cache[row['country']] = country
+
 
             region = None
 
-            if row['region'] != r'\N':
-                region, _ = Region.objects.get_or_create(name=row['region'].split('/')[0])
+            if row['region'] in region_cache:
+                region = region_cache[row['region']]
+            else:
+                if row['region'] != r'\N':
+                    region, _ = Region.objects.get_or_create(name=row['region'].split('/')[0])
+                    region_cache[row['region']] = region
 
             airport = Airport(
                     name=row['name'], code=row['code'] if row['code'] != r'\N' else None,
