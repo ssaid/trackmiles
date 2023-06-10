06e50c6712a433e8de36a17c02a88cb808150000
Dan <danii-benitez@hotmail.com>
2023-06-10 00:43:39 -0300

    [add] google analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,33 @@
+import ReactGA from 'react-ga';
 import bgImage from './assets/bg.jpg'
 import { Countdown } from './components/countdown/Countdown'
 
 function App() {
 
+  const analytics = import.meta.env.VITE_GOOGLE_ANALYTICS_ID ?? false
+  if (analytics){ 
+    ReactGA.initialize(analytics);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+  
+
   return (
 
     <div className="w-auto h-screen relative">
 
-		<div>
-			<img 
-				src={bgImage} 
-				alt="background" 
-				className="absolute object-cover w-full h-full -z-10"
-			/>
-		</div>
-		<section className="bg-black bg-opacity-60 text-white-400 body-font text-white">
-			<div className="container flex flex-col items-center justify-center h-screen px-5 py-24 mx-auto">
-				<Countdown />
-			</div>
-		</section>
-
-
-  </div>
+      <div>
+        <img 
+          src={bgImage} 
+          alt="background" 
+          className="absolute object-cover w-full h-full -z-10"
+        />
+      </div>
+      <section className="bg-black bg-opacity-60 text-white-400 body-font text-white">
+        <div className="container flex flex-col items-center justify-center h-screen px-5 py-24 mx-auto">
+          <Countdown />
+        </div>
+      </section>
+    </div>
 
   )
 }
