c208a931461096fd48cff2d181097ea636eba79a
Santiago J. Said <said.santiago@gmail.com>
2023-06-21 00:37:45 -0300

    [ADD] WaitingList to admin

--- server/flights/admin.py
+++ server/flights/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Preference, Region
+from .models import User, Preference, Region, WaitingList
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Preference)
 admin.site.register(Region)
+admin.site.register(WaitingList)
