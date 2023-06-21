f9858143c5341d614ed25be4d32a4cc1b5950785
Santiago J. Said <said.santiago@gmail.com>
2023-06-21 00:33:20 -0300

    [FIX] Serving static files

--- server/server/settings.py
+++ server/server/settings.py
@@ -131,7 +131,8 @@ class Dev(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
 
-    STATIC_URL = values.Value('/app/static/')
+    STATIC_URL = values.Value('/static/')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # MP_ACCESS_TOKEN = values.SecretValue()
 
