dcc8ad93742efb03de39c1c8b7d0ab988c2aecc8
Santiago J. Said <said.santiago@gmail.com>
2023-07-09 10:35:28 -0300

    [ADD] Missing admin for models

--- server/flights/admin.py
+++ server/flights/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Preference, Region, WaitingList
+from .models import User, Preference, Region, WaitingList, Provider, Flight, FlightHistory
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Preference)
 admin.site.register(Region)
 admin.site.register(WaitingList)
+admin.site.register(Provider)
+admin.site.register(Flight)
+admin.site.register(FlightHistory)
