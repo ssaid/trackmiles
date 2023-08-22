bd60bf5daa8117428827cd385f403c395c21baa1
Dan <danii-benitez@hotmail.com>
2023-08-21 21:12:03 -0300

    [add] sequence field in Faq model

--- server/faqs/admin.py
+++ server/faqs/admin.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 
 from .models import Faq
 
-admin.site.register(Faq)
+# admin.site.register(Faq)
 
-class FaqAdmin(admin.TabularInline):
-    model = Faq
-    fields = ["question", "created_at", "is_published"]
-    ordering = ("-created_at",)
+class FaqAdmin(admin.ModelAdmin):
+    list_display = ["question", "sequence", "created_at", "is_published"]
 
-
-# admin.site.register(Faq, FaqAdmin)
+admin.site.register(Faq, FaqAdmin)
--- server/faqs/models.py
+++ server/faqs/models.py
@@ -8,11 +8,15 @@ class Faq(models.Model):
 
     question = RichTextField()
     answer = RichTextField()
+    sequence = models.IntegerField(default=9999)
     is_published = models.BooleanField(default=False)
 
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['sequence', '-created_at']
 
     def __str__(self):
         return self.question
